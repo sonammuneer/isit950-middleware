1f70521d39606b7e13705fa94925e350540ff4de
Sonam Muneer <sonammoony@gmail.com>
2025-05-08 00:51:24 +0530

    feat: endpoint interceptor added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 const PORT = 3001;
@@ -22,6 +23,28 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.status(200);
   res.send({ response: "hello world!" });
+});
+
+app.all("/*", async (req, res) => {
+  try {
+    const url = `https://isit950-backend.vercel.app${req.originalUrl}`;
+    const config = {
+      method: req.method,
+      url: url,
+      headers: {
+        Authorization: req.headers["authorization"],
+        "Content-Type": req.headers["content-type"],
+      },
+      data: req.body,
+    };
+
+    const response = await axios(config);
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({
+      message: error.message,
+    });
+  }
 });
 
 app.listen(PORT, () => {
